4974623a815088372f4d5cbdc57ca2440df07242
Abenezer Dereje <dabenu2012@gmail.com>
2023-06-26 08:27:18 +0000

    alx-frontend-javascript

---
+++ 0x00-ES6_basic/0-constants.js
@@ -0,0 +1,15 @@
+const taskFirst = () => {
+  const task = 'I prefer const when I can.';
+  return task;
+};
+
+const getLast = () => {
+  return ' is okay';
+};
+
+const taskNext = () => {
+  let combination = 'But sometimes let';
+  combination += getLast();
+
+  return combination;
+};
